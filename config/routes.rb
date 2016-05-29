282cca621389147a30c565131b5d62ecd0b3aaff
Brantley Beaird <bbeaird@gmail.com>
2016-05-28 18:55:33 -0700

    Initial commit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#new'
+  resources :users
+  resources :todos
+  resources :groups
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ db/migrate/20160529001130_create_users_and_groups.rb
@@ -0,0 +1,20 @@
+class CreateUsersAndGroups < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+
+      t.timestamps null: false
+    end
+
+    create_table :groups do |t|
+      t.string :name
+
+      t.timestamps null: false
+    end
+
+    create_table :users_groups, id: false do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :group, index: true
+    end
+  end
+end
